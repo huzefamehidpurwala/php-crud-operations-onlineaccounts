1ed5d613a19639efa7bf1f0bade155010d71b98f
huzefamehidpurwala <jamaliongithub@gmail.com>
2022-04-14 23:49:54 +0530

    first commit

---
+++ connection.php
@@ -0,0 +1,10 @@
+<?php
+
+$dbHost = 'localhost';
+$dbUsername = 'root';
+$dbPassword = '7890654321';
+$dbName = 'test2';
+
+$mysqli = mysqli_connect($dbHost, $dbUsername, $dbPassword, $dbName); 
+	
+?>
---
+++ index.php
@@ -0,0 +1,35 @@
+<?php session_start(); ?>
+<html>
+<head>
+	<title>OnlineAccounts</title>
+	<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+	<center>
+		<div id="header">
+			Welcome to OnlineAccounts!
+		</div>
+		<?php
+		if(isset($_SESSION['valid'])) {			
+			include("connection.php");					
+			// $result = mysqli_query($mysqli, "SELECT * FROM login");
+		?>
+
+			Welcome <?php echo "<b>".$_SESSION['name']."</b>" ?> ! <a href='logout.php'>Logout</a><br/>
+			<br/>
+			<a href='view.php'>View and Add Products</a>
+			<br/><br/>
+		<?php	
+		} else {
+			echo "<h3>You must ";
+			// echo "<em><u><b>log-in</b></u></em>";
+			// echo " or ";
+			// echo "<em><u><b>register</b></u></em>";
+			echo "<a href='login.php'>Login</a> or <a href='register.php'>Register</a>";
+			echo " yourself to continue.<br/><br/></h3>";
+		}
+		?>
+	</center>
+</body>
+</html>
